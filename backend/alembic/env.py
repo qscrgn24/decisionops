036204b2aeac099765b05c6f11fbf48b5583a157
qscrgn24 <singhaniavatsal@gmail.com>
2026-02-15 14:37:42 -0800

    Overwrite alembic.ini to access env db_url

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -1,4 +1,5 @@
 from logging.config import fileConfig
+import os
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
@@ -14,6 +15,9 @@ from app.auth.models import User, OAuthAccount  # Import auth models as well
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+db_url = os.getenv("DATABSE_URL")
+if db_url:
+    config.set_main_option("sqlalchemy.url", db_url)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
